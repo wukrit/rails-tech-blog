0b8de527edab2560783a2fcd58273cbbae3208a7
wukrit <sukritwalia@gmail.com>
2019-08-30 23:55:55 -0400

    Created edit post form

--- rails-tech-blog/app/controllers/posts_controller.rb
+++ rails-tech-blog/app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to @post
+    redirect @post
   end
 
   def destroy
